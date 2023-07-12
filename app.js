694509dabd6e05af6579fa9485e6537b21925c98
AttaTechyKid that loves coding <128951217+AttaTechyKid@users.noreply.github.com>
2023-07-12 15:40:25 -0700

    fix #5 web clip

--- app.js
+++ app.js
@@ -1,25 +1,7 @@
-// Function to scroll to the top of the page
-function scrollToTop() {
-    window.scrollTo(0, 0);
-  }
-  
-  // Function to handle navigation and scroll to top
-  function navigateTo(url) {
-    // Scroll to top before navigating
-    scrollToTop();
-    
-    // Use the History API to change the URL
-    history.pushState(null, null, url);
-    
-    // Prevent the default link behavior
-    event.preventDefault();
-  }
-  
-  // Attach click event handlers to your navigation links
-  const links = document.querySelectorAll('a');
-  links.forEach(link => {
-    link.addEventListener('click', function(event) {
-      navigateTo(link.href);
-    });
-  });
-  
+(function() {
+    var isHomeScreen = window.location.href.indexOf("homescreen://") === 0;
+    if (isHomeScreen) {
+      document.documentElement.style.display = "fullscreen";
+      document.webkitRequestFullScreen();
+    }
+  })(); 
